d2943b225455031d9feabed3c57da2adacd96408
Brian Blaze <brianblaze14@outlook.com>
2023-03-30 21:49:15 +0100

    initial commit

---
+++ src/main/java/com/learning/demo/FlightProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.learning.demo;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class FlightProcessorApplication {
+
+    private static final Logger log = LogManager.getLogger();
+
+    public static void main(String[] args) {
+        log.info("flight-service-details-service-hello::entering......");
+    }
+}
---
+++ src/test/java/com/learning/demo/FlightProcessorApplicationTest.java
@@ -0,0 +1,17 @@
+package com.learning.demo;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FlightProcessorApplicationTest {
+
+    @Before
+    public void init () {
+    }
+
+    @Test
+    public void test() {
+        System.out.println("Testing");
+    }
+}
+
